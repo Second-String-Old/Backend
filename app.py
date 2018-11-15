1e2f2165e0398893c414eb492e04204d57b140bc
cwilliamson226 <colin.williamson@narrativ.com>
2018-11-15 00:45:37 -0500

    Adding dynamic host/port

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def rb():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host=os.environ.get('HOST', '0.0.0.0'), port=port)
