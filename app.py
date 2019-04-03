5642f77e6ccb1443b298637dabddea19d4a6d744
cwilliamson226 <colin.williamson@narrativ.com>
2019-04-03 12:36:01 -0400

    trying to debug addStats

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def makeResponse(payload):
 # Function for adding stats to a dictionary to keep code nice and clean
 # This will have dual use as it will make adding the individual routes way easier/cleaner
 def addStats(dict, stats, pos):
-    if 'full_name' in stats.player.__dict__:
+    if stats.player.full_name:
         if pos == 'QB':
             dict['player_name'] = stats.player.full_name
             dict['player_team'] = stats.team
@@ -72,6 +72,8 @@ def addStats(dict, stats, pos):
             dict['rushing_yds'] = stats.rushing_yds
             dict['rushing_loss_yds'] = stats.rushing_loss_yds
             dict['rushing_tds'] = stats.rushing_tds
+    else:
+        print("ASFDSDGADGSDFGSDGSDFGSDRGF")
     return dict
 
 # /players/QB/?count={count}&year={year}&week={week}
