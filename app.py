35195a9bb7337311dbbb9a7d28a51ab36828afc9
cwilliamson226 <colin.williamson@narrativ.com>
2019-04-09 13:20:41 -0400

    Modifying exception in addStats

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def addStats(dict, stats, pos):
             dict['rushing_tds'] = stats.rushing_tds
         return dict
     except:
-        raise Exception('AddStats failed on player ', stats)
+        raise Exception('AddStats failed on player ', stats.name)
         return None
 
 # /players/QB/?count={count}&year={year}&week={week}
